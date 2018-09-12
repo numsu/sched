0e514f0b9db10229bc285070ec6cd408de2e233e
Miro Metsänheimo <miro@metsanheimo.fi>
2018-09-12 19:30:58 +0300

    style the header a bit

--- app/client/src/app/common/components/header/header.js
+++ app/client/src/app/common/components/header/header.js
@@ -6,9 +6,11 @@ class Header extends Component {
 
     render = () => {
         return (
-            <div className="Header">
+            <div className="header">
+                <div className="logo">
+                    <h2 className="text-logo">Sched</h2>
+                </div>
                 <nav className="navbar">
-                    asd
                 </nav>
             </div>
         );
